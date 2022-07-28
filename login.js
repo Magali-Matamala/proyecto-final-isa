dc71f87c2dbdaf752a03b731b8425d648cdad11b
magali matamala <magalimelisamatamala@outlook.com>
2022-07-28 00:10:47 -0300

    cuarto

--- login.js
+++ login.js
@@ -1,3 +1,17 @@
+
+function acceso (){
+
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
+
+    if(username == "Magalí" && password == 1234 || username == "Pedro" && password == 4321 || username == "María" && password == 4567){
+        alert("Acceso Concedido")
+    }else {
+        alert("Acceso Denegado")
+    }
+}
+
+
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 let button = document.getElementById("button");
@@ -14,5 +28,6 @@ console.log(data);
 alert("INGRESO GUARDADO EN CONSOLA");
 
 })
+
 
 
